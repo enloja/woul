ce1da3ad274d88cd6f3a3c30dad8478efade29fd
Encarnacion Lorry James <enloja@gmail.com>
2019-03-24 21:18:36 -0400

    react app

---
+++ src/main/resources/static/precache-manifest.895bea58a0fd5671c8131f3ecefa7c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "85dae83bef7969632a73",
+    "url": "/static/js/main.b9c48db8.chunk.js"
+  },
+  {
+    "revision": "1f384789fcba1acdc2c2",
+    "url": "/static/js/2.25d36362.chunk.js"
+  },
+  {
+    "revision": "85dae83bef7969632a73",
+    "url": "/static/css/main.8ae0b783.chunk.css"
+  },
+  {
+    "revision": "1f384789fcba1acdc2c2",
+    "url": "/static/css/2.60bc2cdf.chunk.css"
+  },
+  {
+    "revision": "6c1f960d6baf11c58853e667fbf946df",
+    "url": "/index.html"
+  }
+];
